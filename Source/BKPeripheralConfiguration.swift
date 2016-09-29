ba15ac2ee6a4d169db4e70e3d8e2b49122c44017
Jean-Denis Boivin <jd.boivin@cinco.ca>
2016-09-29 18:28:16 -0400

    Update

--- Source/BKPeripheralConfiguration.swift
+++ Source/BKPeripheralConfiguration.swift
@@ -42,4 +42,10 @@ public class BKPeripheralConfiguration: BKConfiguration {
         super.init(dataServiceUUID: dataServiceUUID, dataServiceCharacteristicUUID: dataServiceCharacteristicUUID)
     }
     
+    public init(dataServiceCBUUID: CBUUID, dataServiceCharacteristicCBUUID: CBUUID, localName: String? = nil) {
+        self.localName = localName
+        super.init(dataServiceCBUUID: dataServiceCBUUID, dataServiceCharacteristicCBUUID: dataServiceCharacteristicCBUUID)
+    }
+
+    
 }
